2e7eb5aed80c947baf3f2345253b318ce108ce49
SFenrir20 <santiago_20_97@hotmail.com>
2024-04-12 22:26:00 -0500

    feat: :sparkles: Creacion del proyecto
    se creo las bases para el inicio del soft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './Components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './Components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
